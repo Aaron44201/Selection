cc21c17f399477d90df7323022577b33daf53463
Aaron44201 <44201@longroad.ac.uk>
2014-09-30 12:45:36 +0100

    finished

---
+++ Task 2 class excersises.py
@@ -0,0 +1,11 @@
+#Aaron Bentley
+#30/09/2014
+#dev exercises 2
+
+tempreature = int(input("please enter tempreature of conainer in cenigrade: "))
+if tempreature >= 100:
+    print("boiling")
+elif tempreature <= 0:
+    print("frozen")
+else:
+    print("Neither")
---
+++ Task 4 class excersises.py
@@ -0,0 +1,17 @@
+#Aaron Bentley
+#30/09/2014
+#dev ex 4
+
+grade = int(input("Test score out of 100: "))
+if grade >= 81:
+    print("Grade A")
+elif grade >= 71:
+    print("Grade B")
+elif grade >= 61:
+    print("Grade C")
+elif grade >= 51:
+    print("Grade D")
+elif grade >= 41:
+    print("Grade E")
+else:
+    print("Grade U")
