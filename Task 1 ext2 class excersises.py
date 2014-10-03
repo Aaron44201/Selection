0e98ef955456439858c6c813e84a34576573e1b5
Aaron44201 <44201@longroad.ac.uk>
2014-10-03 14:31:51 +0100

    hw

---
+++ Task 1 ext2 class excersises.py
@@ -0,0 +1,73 @@
+day = int(input("Day: "))
+month = int(input("month: "))
+year = int(input("Year: "))
+if day = 1
+    date_day = 1
+elif day = 2
+    date_day = 2
+elif day = 3
+    date_day = 3
+elif day = 4
+    date_day = 4
+elif day = 5
+    date_day = 5
+elif day = 6
+    date_day = 6
+elif day = 7
+elif day = 8
+elif day = 9
+elif day = 10
+elif day = 11
+elif day = 12
+elif day = 13
+elif day = 14
+elif day = 15
+elif day = 16
+elif day = 17
+elif day = 18
+elif day = 19
+elif day = 20
+elif day = 21
+elif day = 22
+elif day = 23
+elif day = 24
+elif day = 25
+elif day = 26
+elif day = 27
+elif day = 28
+elif day = 29
+elif day = 30
+else:
+    
+if month == 12:
+    print("December")
+elif month == 11:
+    print("November")
+elif month == 10:
+    print("October")
+elif month == 9:
+    print("September")
+elif month == 8:
+    print("August")
+elif month == 7:
+    print("July")
+elif month == 6:
+    print("June")
+elif month == 5:
+    print("May")
+elif month == 4:
+    print("April")
+elif month == 3:
+    print("March")
+elif month == 2:
+    print("February")
+elif month == 1:
+    print("January")
+else:
+    print("{0}This is not a month".format (month))
+
+if year%4 == 0:
+    print("it is a leap year")
+else:
+    print("not a leap year")
+    
